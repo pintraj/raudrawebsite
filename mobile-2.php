ac2d41d8b6fd292b76248c55562f7e1ac349d6e5
pintu <pint.raj850@gmail.com>
2017-04-03 18:32:54 +0530

    Added 2 sec for the mobile version view

--- mobile-2.php
+++ mobile-2.php
@@ -12,7 +12,7 @@
 <div class="row">
     <div class="hidden-md hidden-lg hidden-sm ">
         <br>
-        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="2000">
             <!-- Indicators -->
             <ol class="carousel-indicators">
                 <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
