7dcdc29dd6ebe3eaf8eb1ad09816016e3d9482fa
pintu <pint.raj850@gmail.com>
2017-05-04 14:50:06 +0530

    added focus with corrections

--- focus.php
+++ focus.php
@@ -12,7 +12,7 @@ include "header.php";
     <div class="container">
         <div class="courses-detail-wrapper">
             <div class="row">
-                <div class="col-md-9 layout-left"><h1 class="course-title">Call for the soldiers (Foucs Progame)</h1>
+                <div class="col-md-9 layout-left"><h1 class="course-title">Call for the Soldiers (Focus Program)</h1>
 
                     <div class="course-video">
                         <iframe width="560" height="315" src="https://www.youtube.com/embed/brd91nmHAzc?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
@@ -30,7 +30,7 @@ include "header.php";
 
                             <p>We are providing study space which is pleasant and ideal for studying. We will help in preparing a timetable for the students to organise their preparation for GATE 2018.</p>
 
-                            <p>We will provide you with the Online discussion forum to clarify any doubts and the same forum can be used for learning, communicating and interacting.  They can also be used for peer reviews and assessment of student learning.  Well-posed questions and active participation is the heart of meaningful discussion forums. </p>
+                            <p>We will provide you with the Online discussion forum to clarify any doubts and the same forum can be used for learning, communicating and interacting. They can also be used for peer reviews and assessment of student learning. Well-posed questions and active participation is the heart of meaningful discussion forums. </p>
 
                                 <p>This facility is offered in Hyderabad as of now and Students who want to utilise this facility can reserve their seat. We charge Rs. 100 per day and the student can spend any days as per his convenience. Some can only come for one day or for one week or for the balance 10 months till GATE. All depends on the availability of the seat and the convenience of yours. The facility will be open 24 hrs and the student can come and study anytime of his choice.
                             </p>
@@ -57,19 +57,19 @@ include "header.php";
                         <div class="question">
                             <ol>
                                 <li>How many days are mandatory to stay for this course.?</li>
-                                <p>10 MONTHS. we expect a student to be with us for at least 10 months from June.</p>
+                                <p>10 MONTHS. We expect a student to be with us for at least 10 months from June.</p>
                                 <li>Selection Process?</li>
                                 <p>Register on this form, and since there are limited seats. so the selection priority is as follows
                                     <br>1. Current Gate registered <br>2. Previous Gate Registered students
-                                    <br>3. Current Other course registered student
-                                    <br>4. Non-registered</p>
+                                    <br>3. Current other course registered student
+                                    <br>4. Non-Registered</p>
                                 <li>Facilities provided?</li>
-                                <p>AC, internet, individual workspace.</p>
+                                <p>AC, Internet, Individual workspace.</p>
                                 <li> Fee ?</li>
                                 <p>100 RUPEES per day or you can pay us on monthly basis also.</p>
                                 <li>How will this course be beneficial for you?
                                 </li>
-                                <p>A Student will be preparing Simultaneously for both gate, ML/IOT.You will be exposed to industrial problems and hands-on exposed to solve them.You will be helping other students who are preparing along with you and learning at the same time.</p>
+                                <p>A Student will be preparing simultaneously for both GATE, ML/IOT. You will be exposed to industrial problems and hands-on exposed to solve them. You will be helping other students who are preparing along with you and learning at the same time.</p>
                                 <li>How are classes conducted for ML/IOT. is it offline or online?</li>
                                 <p>OFFLINE, the classes starts from June 15th and one hour per each ML/IOT class and also the same online classes will be provided to you after Aug 15th.</p>
                                 <li>Who is going to teach these courses?</li>
@@ -79,7 +79,7 @@ include "header.php";
                                 <li>Accommodation?</li>
                                 <p>NO, but there are hostels reachable from our workspace.</p>
                                 <li>Certification for ML/IOT?</li>
-                                <p>YES, we provide the certificate on the successful completion of the course and the grades that student acquired during the course work (assignments and assessments) will be reflected on the certificate.</p>
+                                <p>YES, we provide the certificate on the successful completion of the course and the grades that the student acquires during the course work (assignments and assessments) will be reflected on the certificate.</p>
                                 <li>Is it necessary to carry laptops?</li>
                                 <p>YES, you need to carry your own laptop.</p>
 
@@ -99,14 +99,7 @@ include "header.php";
                         </div>
                     </div>
                     <!--venu ends here-->
-                    <!--apply button starts here-->
-                    <div class="ml">
-                        <h2>TO UPGRADE YOUR CAREER</h2>
-                        <a href="#" class="btn btn-outline">Apply Now</a>
 
-                        <!--<button class=" ML btn btn-green" style="text-align: center;"><span>Register</span></button>-->
-                    </div>
-                    <!--apply button ends here-->
                 </div>
                 <!--courses sidebar starts from here-->
                 <div class="col-md-3 sidebar layout-right">
@@ -159,46 +152,24 @@ include "header.php";
                         <div class="course-price-widget widget col-md-12 sd380">
 
                             <div class="content-widget">
-                                <!--<div class="course-price-widget-wrapper">
-                                    <h1 class="title-widget">Fee</h1>
-                                    <div class="#"><p style="font-size: 22px;text-align: center;font-weight: bolder;">INR 18,000 </p></div>
-                                    <div class="row">
-                                        <div class="col-md-12 info-row col-sm-12 col-xs-12">
-                                            <div class="pull-left title">Length:</div>
-                                            <div class="pull-right length">160+</div>
-                                        </div>
-
-
-                                        <div class="col-md-12 info-row col-sm-12 col-xs-12">
-                                            <div class="pull-left title">Validity:</div>
-                                            <div class="pull-right level">1 Year</div>
-                                        </div>
-
-                                    </div>
-                                    <button class="myBtn btn btn-green"><span>Register</span></button>
-                                </div>-->
-                            </div>
-                        </div>
-                        <div class="widget col-sm-6 col-md-12 col-xs-6 sd380">
-                            <h1 class="title-widget">Target Audience </h1>
-                            <div class="row">
-                                <ul>
-                                    <li>B.Tech/B.E in any discipline</li>
-                                    <li>M.Tech/M.E/MS in any discipline</li>
-                                    <li>Candidates who wish to switch from service based companies to product based</li>
-                                    <li>Students who has"Algorithms" in their curriculum</li>
-                                    <li>BCA &amp; MCA</li>
-                                </ul>
 
                             </div>
                         </div>
+
 
                     </div>
                 </div>
 
-            </div>
 
+            </div>
+            <div class="ml">
+                <h2>TO UPGRADE YOUR CAREER</h2>
+                <a href="#" class="btn btn-outline">Apply Now</a>
+
+
+            </div>
         </div>
+
     </div>
 </div>
 <?php
