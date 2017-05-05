2e7ba3d3f78a18dce62aeb5d7f6160fc982f5c56
pintu <pint.raj850@gmail.com>
2017-05-05 12:13:51 +0530

    change the content of focus page

--- focus.php
+++ focus.php
@@ -10,9 +10,9 @@ include "header.php";
 <script src="assets/js/main.js"></script>
 <div class="section section-padding courses-detail">
     <div class="container">
-        <div class="courses-detail-wrapper">
+        <div class="courses-detail-wrapper remove-padding">
             <div class="row">
-                <div class="col-md-9 layout-left"><h1 class="course-title">Call for the Soldiers (Focus Program)</h1>
+                <div class="col-md-12 layout-left"><h1 class="course-title">Call for the Soldiers (Focus Program)</h1>
 
                     <div class="course-video">
                         <iframe width="560" height="315" src="https://www.youtube.com/embed/brd91nmHAzc?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
@@ -22,32 +22,34 @@ include "header.php";
                     <div class="course-des">
                         <div class="course-des-title underline">Course Info</div>
                         <div class="course-des-content">
-                            <p>
-                                We are launching FOCUS BATCH 2018- Limited group of students come together to prepare for GATE 2018. We are launching this for all those students who want to prepare full time, who needs a convenient environment, who needs guidance from experts and who are determined to crack GATE 2018</p>
+                            <p>We are launching FOCUS BATCH 2018 for all those students who want to prepare for GATE full time, who need a convenient environment, who need guidance from experts, who are excited to learn Applied Machine Learning techniques from the industry experts. This program consists the following courses.</p>
 
-                            <p>We are creating an environment where students (GATE aspirants) can come to learn and excel. This is exclusively offered to the registered students who feel that they need a better place to study. This is a dedicated space where students can spend their preparation time by watching the videos in a very structured manner.</p>
+                            <div class="ml-course-info"><ol>
+                                    <li>The focus batch will prepare for GATE from our videos under the supervision of our mentors. We are providing a dedicated study space which is pleasant and ideal for studying. We will help the students in preparing a timetable to organise their preparation for GATE 2018.</li>
+                                    <li>The Applied Machine Learning course will have a classroom experience. The course will be taught by our EXPERT TEAM. We expect our focus batch students to be fully committed to finish our course work and to attend all the classes in person. The complete syllabus for the course will be uploaded soon.</li>
+                                </ol></div>
+
+                            <p>We will provide you with the online discussion forum to clarify any doubts and the same forum can be used for learning, communicating and interacting, and peer reviews. Well-posed questions and active participation is the heart of meaningful discussion forums.</p>
 
 
-                            <p>We are providing study space which is pleasant and ideal for studying. We will help in preparing a timetable for the students to organise their preparation for GATE 2018.</p>
 
-                            <p>We will provide you with the Online discussion forum to clarify any doubts and the same forum can be used for learning, communicating and interacting. They can also be used for peer reviews and assessment of student learning. Well-posed questions and active participation is the heart of meaningful discussion forums. </p>
+                            <p>This facility is offered in Hyderabad as of now and students who want to utilise this facility can reserve their seat. We charge Rs. 100 per day. The facility will be open 24 hrs and the student can come and study anytime of his choice.</p>
 
-                                <p>This facility is offered in Hyderabad as of now and Students who want to utilise this facility can reserve their seat. The facility will be open 24 hrs and the student can come and study anytime of his choice.
-                            </p>
 
 
 
                         </div>
-                        <!--<div class="news-list">
-                            <div class="list-expand-title underline">Pre-Requirements</div>
+                        <div class="news-list">
+                            <div class="list-expand-title underline">Prerequisites</div>
                             <div class="row">
-                                <div class="col-md-12 col-sm-6 col-xs-6">
-                                    <p>Technically there are no prerequisites.
+                                <div class="col-md-12 col-sm-12 col-xs-6">
+                                    <p>There are no prerequisites to get enrolled for this course. But the candidates should carry a laptop when they come to the workspace.
+
                                     </p>
                                 </div>
 
                             </div>
-                        </div>-->
+                        </div>
                     </div>
                     <!--course syllubus -->
 
@@ -56,33 +58,62 @@ include "header.php";
                         <div class="course-des-title underline">FAQ</div>
                         <div class="question">
                             <ol>
-                                <li>How many days are mandatory to stay for this course.?</li>
-                                <p>10 MONTHS. We expect a student to be with us for at least 10 months from June.</p>
-                                <li>Selection Process?</li>
-                                <p>Register by filling the form in the below link,and since there are limited seats. so the selection priority is as follows.
-                                    <br>1. Current Gate registered <br>2. Previous Gate Registered students
-                                    <br>3. Current other course registered student
-                                    <br>4. Non-Registered</p>
-                                <li>Facilities provided?</li>
-                                <p>AC, Internet, Individual workspace.</p>
-                                <li> Fee ?</li>
-                                <p>100 RUPEES per day or you can pay us on monthly basis also.</p>
+                                <li>What is the Total Duration of this course?</li>
+                                <p>We expect a student to be with us for at least 10 months from 15 June, 2017.
+                                </p>
+                                <li>Is it a full time course? Is it mandatory to stay for the whole period?
+                                </li>
+                                <p>Yes, it is a full time course and it is mandatory to stay.</p>
+                                <li>How to apply for this program?
+                                </li>
+                                <p>Please fill this form.
+                                </p>
+                                <li>Is there any selection criteria?
+                                </li>
+                                <p>Yes and moreover as there are limited seats, there is a selection priority as follows: <br>
+                                   A. Current GATE registered students <br>
+                                   B. Previous GATE registered students <br>
+                                   C. Current other course registered students <br>
+                                   D. Others
+
+                                </p>
+                                <li>Facilities provided?
+                                </li>
+                                <p>AC Study Rooms, Uninterrupted High Speed Internet, Wifi Access, Individual workspace.
+
+
+                                </p>
+                                <li> What is the Fees for this course?
+                                </li>
+                                <p>We charge Rs. 100 per day or you can pay us on monthly basis also. We leave this to your discretion.</p>
+                                <li>Will there be periodic assessment of the student?
+                                </li>
+                                <p>Yes, there will be periodic assessment of the student. This course requires high level of involvement from the students for the complete duration of the course. At any time during the course, if our team believes that you are not meeting the expectations of the course, you may be asked to leave the course.
+                                </p>
                                 <li>How will this course be beneficial for you?
                                 </li>
-                                <p>A Student will be preparing simultaneously for both GATE, ML/IOT. You will be exposed to industrial problems and hands-on exposed to solve them. You will be helping other students who are preparing along with you and learning at the same time.</p>
-                                <li>How are classes conducted for ML/IOT. is it offline or online?</li>
-                                <p>OFFLINE, the classes starts from June 15th and one hour per each ML/IOT class and also the same online classes will be provided to you after Aug 15th.</p>
-                                <li>Who is going to teach these courses?</li>
-                                <p>Our team.</p>
-                                <li>Is it necessary to sit in these courses ML/IOT?</li>
-                                <p>YES, it is mandatory to attend the classes and complete the assignments. These assignments will be announced during the classes.</p>
-                                <li>Accommodation?</li>
-                                <p>NO, but there are hostels reachable from our workspace.</p>
-                                <li>Certification for ML/IOT?</li>
-                                <p>YES, we provide the certificate on the successful completion of the course and the grades that the student acquires during the course work (assignments and assessments) will be reflected on the certificate.</p>
-                                <li>Is it necessary to carry laptops?</li>
-                                <p>YES, you need to carry your own laptop.</p>
+                                <p> A student will be preparing simultaneously for both GATE and ML. You will be exposed to Industrial problems / situations and you will get the hands-on experience to solve them.
+                                </p>
+                                <li>How are classes conducted for ML? Is it offline or online? What is the course commencement Date?
+                                </li>
+                                <p>The classes for ML are OFFLINE. The classes will start from 15 June, 2017.
+                                </p>
+                                <li>Is it necessary to sit in ML course?</li>
+                                <p>Yes, it is mandatory to attend the classes of ML and complete the assignments. These assignments will be announced during the respective classes.
 
+                                </p>
+                                <li>Accommodation?
+                                </li>
+                                <p>No, we don’t provide any accommodation but there are hostels around our workplace.
+                                </p>
+                                <li>Certification for ML?
+                                </li>
+                                <p>Yes, we provide the certificate on the successful completion of the course and the grades that student acquired during the course work (assignments and exams) will be reflected on the certificate.
+                                </p>
+                                <li>Is it necessary to carry laptops?
+                                </li>
+                                <p>Yes, you need to carry your own laptop.
+                                </p>
                             </ol>
                         </div>
 
@@ -93,7 +124,7 @@ include "header.php";
                         <div class="list-expand-title underline">Venue</div>
                         <div class="row">
                             <div class="col-md-12 col-sm-6 col-xs-6">
-                                <p><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3805.9430707195497!2d78.35052001487742!3d17.462438688033693!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb93b3f0000001%3A0x57dd07d512be374b!2sRaudra+EduServices+Pvt.Limited!5e0!3m2!1sen!2sin!4v1493835253014" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe></p>
+                                <div class="raudra-map"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3805.9430707195497!2d78.35052001487742!3d17.462438688033693!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb93b3f0000001%3A0x57dd07d512be374b!2sRaudra+EduServices+Pvt.Limited!5e0!3m2!1sen!2sin!4v1493835253014" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe></div>
                             </div>
 
                         </div>
@@ -102,75 +133,20 @@ include "header.php";
 
                 </div>
                 <!--courses sidebar starts from here-->
-                <div class="col-md-3 sidebar layout-right">
-                    <div class="row">
 
-
-                        <div class="clearfix"></div>
-                        <div class="popular-course-widget widget col-md-12 sd380">
-                            <div class="title-widget">Popular Courses</div>
-                            <div class="content-widget">
-                                <div class="media">
-                                    <div class="media-left"><a href="gate-2018.php" class="link"><img src="assets/images/latestcourse/GATE.png" alt="" class="media-image"/></a></div>
-                                    <div class="media-right vertical-middle-custom">
-                                        <a href="gate-2018.php" class="link title">GATE</a>
-
-
-
-                                    </div>
-                                </div>
-                                <div class="media">
-                                    <div class="media-left"><a href="java.php" class="link"><img src="assets/images/latestcourse/java.png" alt="" class="media-image"/></a></div>
-                                    <div class="media-right vertical-middle-custom">
-                                        <a href="java.php" class="link title">JAVA</a>
-
-
-
-                                    </div>
-                                </div>
-                                <div class="media">
-                                    <div class="media-left"><a href="btech-projects.php" class="link"><img src="assets/images/latestcourse/proj.png" alt="" class="media-image"/></a></div>
-                                    <div class="media-right vertical-middle-custom">
-                                        <a href="btech-projects.php" class="link title">Projects</a>
-
-
-
-                                    </div>
-                                </div>
-                                <div class="media">
-                                    <div class="media-left"><a href="hadoop.php" class="link"><img src="assets/images/latestcourse/raudraHadoop.png" alt="" class="media-image"/></a></div>
-                                    <div class="media-right vertical-middle-custom">
-                                        <a href="hadoop.php" class="link title">Hadoop</a>
-
-
-
-                                    </div>
-                                </div>
-
-                            </div>
-                        </div>
-                        <div class="course-price-widget widget col-md-12 sd380">
-
-                            <div class="content-widget">
-
-                            </div>
-                        </div>
-
-
-                    </div>
-                </div>
 
 
             </div>
-            <div class="ml">
-                <h2>TO UPGRADE YOUR CAREER</h2>
-                <a href="#" class="btn btn-outline">Apply Now</a>
 
-
-            </div>
         </div>
 
     </div>
+</div>
+<div class="ml">
+    <h1>TO UPGRADE YOUR CAREER</h1>
+    <a href="#" class="btn btn-outline">Apply Now</a>
+
+
 </div>
 <?php
 include "footer.php";
